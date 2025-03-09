46e9c62af3d0a1c724fc8782c6831b761394f0f8
Aurelien Gremy <gremy.aurelien@gmail.com>
2025-03-09 16:34:50 -0400

    [ CreatePost ]: Component for new post with Toast Notif - Not image yet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
-export default function Home() {
+import CreatePost from "@/components/CreatePost";
+import { currentUser } from "@clerk/nextjs/server";
+
+export default async function Home() {
+
+  const user = await currentUser();
   return (
-    <div>
-      home page
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+        {user ? <CreatePost /> : null}
+      </div>
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        WhoToFollow
+      </div>
     </div>
   );
 }
