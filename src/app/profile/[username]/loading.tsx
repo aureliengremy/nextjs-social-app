eadc8302c8d65e9ab9fefc5637cc8ee9d42de4f3
Aurelien Gremy <gremy.aurelien@gmail.com>
2025-05-23 17:03:35 -0400

    [ Profile page - Error and Loading ] add the dynamic profile page with error and loading component

---
+++ src/app/profile/[username]/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function loading() {
+    return (
+        <div>Loarding...</div>
+    )
+}
+
+export default loading
---
+++ src/app/profile/[username]/page.tsx
@@ -0,0 +1,17 @@
+
+
+async function ProfilePage({ params }: { params: { username: string } }) {
+    // console.log("Params: ", params)
+
+    // Try error component :
+    // throw new Error()
+
+    //Try the loading component :
+    // await new Promise((resolve) => setTimeout(resolve, 3000))
+
+    return (
+        <div>ProfilePage</div>
+    )
+}
+
+export default ProfilePage
