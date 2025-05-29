19d81b9755b3caa66699589c03b1b3df9bbfc543
Aurelien Gremy <gremy.aurelien@gmail.com>
2025-05-29 19:28:21 -0400

    [ Dyna Profile Page Action & Metadata] all the necessary actions for profile page with metadata function

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,3 +1,16 @@
+import { getProfileByUsername } from "@/actions/profile.actions"
+
+
+export async function generateMetadata({ params }: { params: { username: string } }) {
+    const user = await getProfileByUsername(params.username)
+    if (!user) return;
+
+    return {
+        title: `${user.name ?? user.username}`,
+        description: user.bio ?? `No description for ${user.username}`,
+    }
+}
+
 
 
 async function ProfilePage({ params }: { params: { username: string } }) {
