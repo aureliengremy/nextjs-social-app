6f90a2f35df1c70f673e15f9c9b96a0ceeec0a13
Aurelien Gremy <gremy.aurelien@gmail.com>
2025-06-09 18:13:46 -0400

    [ Profile Page & Edit ] Edit profile and receive follow, liked and related Posts

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,30 +1,53 @@
-import { getProfileByUsername } from "@/actions/profile.actions"
+import {
+  getProfileByUsername,
+  getUserLikedPosts,
+  getUserPosts,
+  isFollowing,
+} from "@/actions/profile.actions";
+import { notFound } from "next/navigation";
+import ProfilePageClient from "./ProfilePageClient";
 
+export async function generateMetadata({
+  params,
+}: {
+  params: { username: string };
+}) {
+  const user = await getProfileByUsername(params.username);
+  if (!user) return;
 
-export async function generateMetadata({ params }: { params: { username: string } }) {
-    const user = await getProfileByUsername(params.username)
-    if (!user) return;
-
-    return {
-        title: `${user.name ?? user.username}`,
-        description: user.bio ?? `No description for ${user.username}`,
-    }
+  return {
+    title: `${user.name ?? user.username}`,
+    description: user.bio ?? `No description for ${user.username}`,
+  };
 }
-
-
 
 async function ProfilePage({ params }: { params: { username: string } }) {
-    // console.log("Params: ", params)
+  // console.log("Params: ", params)
 
-    // Try error component :
-    // throw new Error()
+  // Try error component :
+  // throw new Error()
 
-    //Try the loading component :
-    // await new Promise((resolve) => setTimeout(resolve, 3000))
+  //Try the loading component :
+  // await new Promise((resolve) => setTimeout(resolve, 3000))
 
-    return (
-        <div>ProfilePage</div>
-    )
+  const user = await getProfileByUsername(params.username);
+
+  if (!user) notFound();
+
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+    getUserPosts(user.id),
+    getUserLikedPosts(user.id),
+    isFollowing(user.id),
+  ]);
+
+  return (
+    <ProfilePageClient
+      user={user}
+      posts={posts}
+      likedPosts={likedPosts}
+      isFollowing={isCurrentUserFollowing}
+    />
+  );
 }
 
-export default ProfilePage
+export default ProfilePage;
